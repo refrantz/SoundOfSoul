1e01a37efb1faa75daa163557e272df97e5b5104
refrantz <renan262001@gmail.com>
2024-05-30 17:47:23 -0300

    update with forks and remove unecessary bloat

---
+++ setup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/refrantz/text-to-text-transfer-transformer_singsong
+git clone https://github.com/refrantz/t5x_singsong
+git clone https://github.com/google/airio
+pip install -e ./airio
+pip install -e ./t5x_singsong
+pip install -e ./text-to-text-transfer-transformer_singsong
